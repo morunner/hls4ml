3ed21356dcdcd172df23862db59c446544debc1c
morunner <107193613+morunner@users.noreply.github.com>
2026-01-20 08:38:17 +0100

    hack conv1d latency to allow setting II
        via TIME_STEP variable
        improve II for garnet evaluation

--- hls4ml/templates/vitis/nnet_utils/nnet_conv1d_latency.h
+++ hls4ml/templates/vitis/nnet_utils/nnet_conv1d_latency.h
@@ -12,74 +12,80 @@ void conv_1d_latency_cl(data_T data[CONFIG_T::in_width * CONFIG_T::n_chan],
                         res_T res[CONFIG_T::out_width * CONFIG_T::n_filt],
                         typename CONFIG_T::weight_t weights[CONFIG_T::filt_width * CONFIG_T::n_chan * CONFIG_T::n_filt],
                         typename CONFIG_T::bias_t biases[CONFIG_T::n_filt]) {
+
     constexpr unsigned mult_n_in = CONFIG_T::filt_width * CONFIG_T::n_chan;
     constexpr unsigned mult_n_out = CONFIG_T::n_filt;
 
-    data_T data_buf[CONFIG_T::n_pixels][mult_n_in];
-    #pragma HLS ARRAY_PARTITION variable=data_buf complete dim=0
+    constexpr int TIME_STEPS = 32;
+    constexpr int PARALLEL_FACTOR = CONFIG_T::n_partitions / TIME_STEPS; 
 
-    typename CONFIG_T::accum_t mult[mult_n_in * mult_n_out];
-    #pragma HLS ARRAY_PARTITION variable=mult complete
-
-    typename CONFIG_T::accum_t acc[mult_n_out];
-    #pragma HLS ARRAY_PARTITION variable=acc complete
+    #pragma HLS ARRAY_PARTITION variable=data type=cyclic factor=PARALLEL_FACTOR
+    #pragma HLS ARRAY_PARTITION variable=res type=cyclic factor=PARALLEL_FACTOR
 
     #pragma HLS ARRAY_PARTITION variable=weights complete
     #pragma HLS ARRAY_PARTITION variable=biases complete
 
-    // Limit multipliers to control parallelization
-    #pragma HLS ALLOCATION operation instances=mul limit=CONFIG_T::mult_config::multiplier_limit
+TimeLoop:
+    for (int t = 0; t < TIME_STEPS; t++) {
+        #pragma HLS PIPELINE II=1 rewind
 
-PartitionLoop:
-    for (int i_part = 0; i_part < CONFIG_T::n_partitions; i_part++) {
-        #pragma HLS PIPELINE II=CONFIG_T::reuse_factor rewind
-
-        CONFIG_T::template fill_buffer<data_T, CONFIG_T>::fill_buffer(data, data_buf, i_part);
-
-    PixelLoop:
-        for (unsigned i_pxl = 0; i_pxl < CONFIG_T::n_pixels; i_pxl++) {
+    ParallelLoop:
+        for (int p = 0; p < PARALLEL_FACTOR; p++) {
             #pragma HLS UNROLL
 
-            data_T cache;
+            int i_part = t * PARALLEL_FACTOR + p;
 
-        // Do the matrix-multiply
-        Product1:
-            for (int i_in = 0; i_in < mult_n_in; i_in++) {
+            data_T data_buf[CONFIG_T::n_pixels][mult_n_in];
+            #pragma HLS ARRAY_PARTITION variable=data_buf complete dim=0
+
+            typename CONFIG_T::accum_t mult[mult_n_in * mult_n_out];
+            #pragma HLS ARRAY_PARTITION variable=mult complete
+
+            typename CONFIG_T::accum_t acc[mult_n_out];
+            #pragma HLS ARRAY_PARTITION variable=acc complete
+
+            CONFIG_T::template fill_buffer<data_T, CONFIG_T>::fill_buffer(data, data_buf, i_part);
+
+        PixelLoop:
+            for (unsigned i_pxl = 0; i_pxl < CONFIG_T::n_pixels; i_pxl++) {
                 #pragma HLS UNROLL
-                cache = data_buf[i_pxl][i_in];
-            Product2:
-                for (int i_out = 0; i_out < mult_n_out; i_out++) {
+
+                data_T cache;
+
+            Product1:
+                for (int i_in = 0; i_in < mult_n_in; i_in++) {
                     #pragma HLS UNROLL
-                    mult[i_in * mult_n_out + i_out] =
-                        CONFIG_T::mult_config::template product<data_T, typename CONFIG_T::mult_config::weight_t>::product(
-                            cache, weights[i_in * mult_n_out + i_out]);
+                    cache = data_buf[i_pxl][i_in];
+                Product2:
+                    for (int i_out = 0; i_out < mult_n_out; i_out++) {
+                        #pragma HLS UNROLL
+                        mult[i_in * mult_n_out + i_out] =
+                            CONFIG_T::mult_config::template product<data_T, typename CONFIG_T::mult_config::weight_t>::product(
+                                cache, weights[i_in * mult_n_out + i_out]);
+                    }
                 }
-            }
 
-        // Initialize accumulator with input biases
-        ResetAccum:
-            for (int i_acc = 0; i_acc < mult_n_out; i_acc++) {
-                #pragma HLS UNROLL
-                acc[i_acc] = (typename CONFIG_T::accum_t)biases[i_acc];
-            }
-
-        // Accumulate multiplication result
-        Accum1:
-            for (int i_in = 0; i_in < mult_n_in; i_in++) {
-                #pragma HLS UNROLL
-            Accum2:
-                for (int i_out = 0; i_out < mult_n_out; i_out++) {
+            ResetAccum:
+                for (int i_acc = 0; i_acc < mult_n_out; i_acc++) {
                     #pragma HLS UNROLL
-                    acc[i_out] += mult[i_in * mult_n_out + i_out];
+                    acc[i_acc] = (typename CONFIG_T::accum_t)biases[i_acc];
                 }
-            }
+            Accum1:
+                for (int i_in = 0; i_in < mult_n_in; i_in++) {
+                    #pragma HLS UNROLL
+                Accum2:
+                    for (int i_out = 0; i_out < mult_n_out; i_out++) {
+                        #pragma HLS UNROLL
+                        acc[i_out] += mult[i_in * mult_n_out + i_out];
+                    }
+                }
 
-        // Cast to "res_t" type
-        Result:
-            for (int i_res = 0; i_res < mult_n_out; i_res++) {
-                #pragma HLS UNROLL
-                res[i_part * CONFIG_T::n_pixels * mult_n_out + i_pxl * mult_n_out + i_res] =
-                    cast<data_T, res_T, typename CONFIG_T::mult_config>(acc[i_res]);
+            Result:
+                for (int i_res = 0; i_res < mult_n_out; i_res++) {
+                    #pragma HLS UNROLL
+                    res[i_part * CONFIG_T::n_pixels * mult_n_out + i_pxl * mult_n_out + i_res] =
+                        cast<data_T, res_T, typename CONFIG_T::mult_config>(acc[i_res]);
+                }
             }
         }
     }
